b9a792a03610a19cc81a4bee43f9f3ebb161c963
Nikola Nikolas <n.nikolas92@outlook.com>
2022-01-23 15:43:36 +0100

    Setup project

---
+++ SmsAPI/SmsAPI.Infrastructure/EntityConfiguration/SMSResponsesEntityTypeConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SmsAPI.Domain.Models;
+using System;
+
+namespace SmsAPI.Infrastructure.EntityConfiguration
+{
+    public class SMSResponsesEntityTypeConfiguration : IEntityTypeConfiguration<SMSResponses>
+    {
+        public void Configure(EntityTypeBuilder<SMSResponses> builder)
+        {
+            GuardBuilder(builder);
+            builder.HasKey(c => c.Id);
+        }
+        private static void GuardBuilder(EntityTypeBuilder<SMSResponses> builder)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+        }
+    }
+}
---
+++ SmsAPI/SmsAPI.Infrastructure/SMSApiContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmsAPI.Domain.Models;
+using SmsAPI.Infrastructure.EntityConfiguration;
+
+namespace SmsAPI.Infrastructure
+{
+    public class SMSApiContext : DbContext
+    {
+        public SMSApiContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        DbSet<SMSResponses> SMSResponses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfiguration(new SMSResponsesEntityTypeConfiguration());
+        }
+    }
+}
